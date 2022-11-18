08b04a8ff64f068cc3903c393b8e1f82101e9a78
Rodrigo Gamero <rodrigo.gamero.luque@gmail.com>
2022-11-18 12:11:21 +0100

    Back started

---
+++ backend/VehiclesApi/VehiclesApi/Models/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace VehiclesApi.Models
+{
+    public class Owner
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string? DriverLicense { get; set; }
+    }
+}
